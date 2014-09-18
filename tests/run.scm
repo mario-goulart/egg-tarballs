5a68c74a367a6a7cc1bedc6bf3a049dce07df390
Mario Domenech Goulart <mario.goulart@gmail.com>
2014-09-18 20:55:47 -0300

    Add tests for the new henrietta-cache cache format

--- tests/run.scm
+++ tests/run.scm
@@ -8,9 +8,11 @@
    "egg-tarballs"))
 
 (system* "rm -rf tarballs")
-(system* (sprintf "~a henrietta-cache tarballs" egg-tarballs))
+(system* (sprintf "~a henrietta-cache-old-format tarballs" egg-tarballs))
 
 (test-begin "egg-tarballs")
+
+(test-begin "old cache format")
 
 (test "tarballs/foo/foo-1.0.tar.gz"
       (file-exists? "tarballs/foo/foo-1.0.tar.gz"))
@@ -20,6 +22,26 @@
 
 (test (sha1sum "tarballs/foo/foo-1.0.tar.gz")
       (with-input-from-file "tarballs/foo/foo-1.0.tar.gz.sha1" read-line))
+
+(test-end "old cache format")
+
+
+(system* "rm -rf tarballs")
+(system* (sprintf "~a henrietta-cache-new-format tarballs" egg-tarballs))
+
+(test-begin "new cache format")
+
+(test "tarballs/foo/foo-1.0.tar.gz"
+      (file-exists? "tarballs/foo/foo-1.0.tar.gz"))
+
+(test "tarballs/foo/foo-1.0.tar.gz.sha1"
+      (file-exists? "tarballs/foo/foo-1.0.tar.gz.sha1"))
+
+(test (sha1sum "tarballs/foo/foo-1.0.tar.gz")
+      (with-input-from-file "tarballs/foo/foo-1.0.tar.gz.sha1" read-line))
+
+(test-end "new cache format")
+
 
 (test-end "egg-tarballs")
 
